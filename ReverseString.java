910b90def09eb32ba5712b5c8a88552899b8eba8
sabari605 <124019510+sabari605@users.noreply.github.com>
2024-12-03 13:13:42 +0530

    day7

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+package day7;
+
+public class ReverseString {
+	public static void main(String[] agrs) {
+for (int i=0; i<=8;i++) {
+	
+for (int j=0; j<=5;j++) {
+	System.out.print("*");
+	
+}	
+	
+System.out.println();
+}
+		
+		
+	}
+
+}
---
+++ VaranamAyiram.java
@@ -0,0 +1,23 @@
+package day7;
+
+import java.util.Scanner;
+
+public class VaranamAyiram {
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	String name= sc.nextLine();
+	if(name.equals("Alive")) {
+		System.out.println("suriya Weds Mehgana");
+	}
+	else {
+		System.out.println("Suriya Wed Ramya");
+	}
+}	
+	
+	
+	
+	
+	
+	
+
+}
